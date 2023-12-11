31836bd60e0aaeef332704aa81570c100a977be3
javier-martin-avila <martinavilajavier@gmail.com>
2023-12-11 17:28:55 +0100

    Arrays terminados

---
+++ S1/3.arrays/exercise-5.js
@@ -0,0 +1,4 @@
+const RickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+RickAndMortyCharacters.shift();
+console.log(RickAndMortyCharacters[0]);
+console.log(RickAndMortyCharacters[4]);
